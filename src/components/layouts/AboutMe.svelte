8a2d4426934de4175b1f4daf7ec4775c47d22ce7
ihell <ihell269@gmail.com>
2024-08-23 21:14:36 +0700

    push

---
+++ src/components/layouts/AboutMe.svelte
@@ -0,0 +1,10 @@
+<section class="pt-6 md:pt-10 p-6 mt-5">
+  <h2
+    class="text-xl md:text-2xl font-bold pb-4 text-gray-800 border-b-2 border-blue-500"
+  >
+    About Me
+  </h2>
+  <p class="text-sm md:text-base leading-relaxed text-gray-700">
+   I am a web developer. and master several programming languages ​​such as JavaScript, PHP and Python. I am a student from SMK Plus Insan Cemerlang with a program majoring in software engineering
+  </p>
+</section>
